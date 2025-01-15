b02b1fdac6a3b651459aab747d3cace9a3c03543
82L <16747176+82L@users.noreply.github.com>
2025-01-15 14:32:38 +0100

    finished 8 ball

--- MagicBallFunction.cs
+++ MagicBallFunction.cs
@@ -6,32 +6,58 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text.Json;
+using Azure;
+using Azure.AI.OpenAI;
+using OpenAI.Chat;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace MagicBall.Function
 {
     public class MagicBallFunction
     {
+        
+        private static readonly string[] MagicBallAnswers = [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful.",
+        ];
 
 
         [FunctionName("MagicBallFunction")]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", "put", Route = null)] HttpRequest req, ILogger log)
         {
-            var timeStamp = DateTime.UtcNow;
-            log.LogInformation(timeStamp + ": Processing Speech-to-Text request.");
+            log.LogInformation("Processing Speech-to-Text request.");
 
             string speechSubscriptionKey = Environment.GetEnvironmentVariable("SPEECH_KEY");
             string openAiApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+            string openAiApiEndpoint = Environment.GetEnvironmentVariable("OPENAI_API_ENDPOINT");
+            string aiInstructions= Environment.GetEnvironmentVariable("AI_INSTRUCTIONS");
             string serviceRegion = "francecentral";
 
-            if (string.IsNullOrEmpty(speechSubscriptionKey) || string.IsNullOrEmpty(openAiApiKey))
+            if (string.IsNullOrEmpty(speechSubscriptionKey) || string.IsNullOrEmpty(openAiApiKey) 
+            || string.IsNullOrEmpty(openAiApiEndpoint) || string.IsNullOrEmpty(aiInstructions))
             {
-                log.LogError(timeStamp + ": Missing required API keys.");
+                log.LogError("Missing required API keys.");
                 return new NoContentResult();
             }
 
@@ -39,7 +65,7 @@ namespace MagicBall.Function
             var file = req.Form.Files.GetFile("audioFile");
             if (file == null || file.Length == 0)
             {
-                return new BadRequestObjectResult(timeStamp + ": Missing AudioFile");
+                return new BadRequestObjectResult("Missing AudioFile");
             }
 
             var tempFilePath = Path.Combine(Path.GetTempPath(), Path.GetFileName(file.FileName));
@@ -48,82 +74,95 @@ namespace MagicBall.Function
                 await file.CopyToAsync(fileStream);
             }
 
-            using AudioConfig audioConfig = AudioConfig.FromWavFileInput(tempFilePath);
             var speechConfig = SpeechConfig.FromSubscription(speechSubscriptionKey, serviceRegion);
             speechConfig.SpeechRecognitionLanguage = "fr-FR";
-            // speechConfig.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Raw16Khz16BitMonoPcm);
+            speechConfig.SpeechSynthesisVoiceName = "en-US-OnyxTurboMultilingualNeural";
 
-            using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
-            var recognitionResult = await recognizer.RecognizeOnceAsync();
-
-            // Créer un objet SpeechRecognizer
 
             bool skipOpenAi = false;
             string aiResponse = "Voix non reconnue.";
 
-            if (recognitionResult.Reason != ResultReason.RecognizedSpeech)
+            string recognizedText = await SpeechToText(tempFilePath, speechConfig);
+            if (string.IsNullOrEmpty(recognizedText))
             {
-                log.LogError(timeStamp + ": Voix non reconnue.");
                 aiResponse = "Voix non reconnue.";
+                log.LogInformation("Voix non reconnue !");
                 skipOpenAi = true;
             }
 
-
-            string recognizedText = recognitionResult.Text;
-            log.LogInformation(timeStamp + $": Recognized Text: {recognizedText}");
+            log.LogInformation($"Recognized Text: {recognizedText}");
             if (!skipOpenAi)
             {
-
-
-                // Send recognized text to OpenAI API
-                using var httpClient = new HttpClient();
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", openAiApiKey);
-
-                var openAiRequest = new
-                {
-                    model = "gpt-3.5-turbo",
-                    messages = new[]
-                    {
-                            new { role = "system", content = "You are a seer able to see the future, with an existential dread about it because of the climate change and the incoming apocalypse caused by it. You must speak in french." },
-                            new { role = "user", content = recognizedText }
-                        },
-                    temperature = 0.7
-                };
-
-                var openAiResponse = await httpClient.PostAsJsonAsync("https://api.openai.com/v1/chat/completions", openAiRequest);
-                if (!openAiResponse.IsSuccessStatusCode)
-                {
-                    log.LogError(timeStamp + ": Open AI ne fonctionne pas");
-                    aiResponse = "La boule magique est cassée";
-                }
-                else
-                {
-                    var openAiResponseContent = await openAiResponse.Content.ReadAsStringAsync();
-                    var openAiResult = JsonSerializer.Deserialize<JsonElement>(openAiResponseContent);
-                    aiResponse = openAiResult.GetProperty("choices")[0].GetProperty("message").GetProperty("content").GetString();
-                    log.LogInformation(timeStamp + $": AI Response: {aiResponse}");
-
-                }
-
-
+                log.LogInformation("Trying to get Ai reponse");
+                aiResponse = GetAIResponse(recognizedText,aiInstructions, openAiApiKey, openAiApiEndpoint, log);
             }
+
+            log.LogInformation($"Ai reponse :{aiResponse}");
             // Convert AI response to speech
-            using var synthesizer = new SpeechSynthesizer(speechConfig);
-            using var stream = AudioOutputStream.CreatePullStream();
-            speechConfig.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Raw16Khz16BitMonoPcm);
 
-            var synthesisResult = await synthesizer.SpeakTextAsync(aiResponse);
-            if (synthesisResult.Reason != ResultReason.SynthesizingAudioCompleted)
+
+            var audioBytes = await TextToSpeech(aiResponse, speechConfig);
+
+            if (audioBytes == null)
             {
-                return new BadRequestObjectResult(timeStamp + ": Error generating audio from AI response.");
+                return new BadRequestObjectResult("Error generating audio from AI response.");
             }
-            var audioBytes = synthesisResult.AudioData;
 
             // Return the audio file
             return new FileContentResult(audioBytes, "audio/wav")
             {
                 FileDownloadName = "response.wav"
             };
+        }
+        private static async Task<string> SpeechToText(string tempFilePath, SpeechConfig speechConfig)
+        {
+            using AudioConfig audioConfig = AudioConfig.FromWavFileInput(tempFilePath);
+
+            using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+            var recognitionResult = await recognizer.RecognizeOnceAsync();
+            if (recognitionResult.Reason != ResultReason.RecognizedSpeech)
+            {
+                return string.Empty;
+            }
+            return recognitionResult.Text;
+        }
+
+        private static async Task<byte[]> TextToSpeech(string text, SpeechConfig speechConfig)
+        {
+            using var synthesizer = new SpeechSynthesizer(speechConfig);
+            using var stream = AudioOutputStream.CreatePullStream();
+            speechConfig.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Raw16Khz16BitMonoPcm);
+            var synthesisResult = await synthesizer.SpeakTextAsync(text);
+
+            if (synthesisResult.Reason != ResultReason.SynthesizingAudioCompleted)
+            {
+                return null;
+            }
+            return synthesisResult.AudioData;
+
+        }
+
+        private static string GetAIResponse(string prompt, string aiInstructions, string key, string endpoint, ILogger log)
+        {
+            int reponseNumber = RandomNumberGenerator.GetInt32(0, MagicBallAnswers.Length);
+            string aiInstructionsText = aiInstructions.Replace("<answer>", MagicBallAnswers[reponseNumber]);
+            log.LogInformation("Creating Key");
+            AzureKeyCredential credential = new(key);
+            log.LogInformation($"Creating Client {endpoint}" );
+            AzureOpenAIClient azureClient = new(new Uri(endpoint), credential);
+            log.LogInformation("Getting Client");
+            ChatClient chatClient = azureClient.GetChatClient("gpt-35-turbo-16k");
+            log.LogInformation($"Created client {prompt}, {endpoint}");
+            ChatCompletion completion = chatClient.CompleteChat(
+                new ChatMessage[]
+                {
+                new SystemChatMessage(
+                    aiInstructionsText ),
+                new UserChatMessage(prompt)
+                }
+            );
+            log.LogInformation("messageSent");
+            return completion.Content.Last().Text;
         }
     }
 }
